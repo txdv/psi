1c4b9ea51e85be68fdff1f4645e6bf307a16046d
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-05-05 02:46:56 +0200

    Pretty printing.

--- Psi.App/Main.cs
+++ Psi.App/Main.cs
@@ -1,22 +1,3 @@
-/*
-  This file is part of Psi.
-
-  Copyright (C) 2011 Andrius Bentkus
-
-  Psi is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Foobar is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with Psi.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,139 +5,144 @@ using Psi;
 
 namespace Psi.App
 {
-  public class Benchmark
-  {
-    private static List<double> times = new List<double>();
-    private static int currentLineNumber = -1;
+	public class Benchmark
+	{
+		private static List<double> times = new List<double>();
+		private static int currentLineNumber = -1;
 
-    public static void ReadDirectory(string directory)
-    {
-      DateTime dt = DateTime.Now;
-      DirectoryInfo di = new DirectoryInfo(directory);
+		public static void ReadDirectory(string directory)
+		{
+			DateTime dt = DateTime.Now;
+			DirectoryInfo di = new DirectoryInfo(directory);
 
-      foreach (FileInfo fi in di.GetFiles()) {
-        ReadFile(fi);
-      }
+			foreach (FileInfo fi in di.GetFiles()) {
+				ReadFile(fi);
+			}
 
-      currentLineNumber = -1;
+			currentLineNumber = -1;
 
-      double duration = (DateTime.Now - dt).TotalSeconds;
-      times.Add(duration);
-      Console.WriteLine("Total time: {0}s", duration);
-    }
+			double duration = (DateTime.Now - dt).TotalSeconds;
+			times.Add(duration);
+			Console.WriteLine("Total time: {0}s", duration);
+		}
 
-    public static void ReadFile(FileInfo fi)
-    {
-      //DateTime dt;
-      StreamReader sr = null;
-      try {
-        sr = new StreamReader(File.Open(fi.FullName, FileMode.Open));
-        currentLineNumber = 0;
-        //dt = DateTime.Now;
-        while (!sr.EndOfStream) {
-          currentLineNumber++;
-          ReadLine(sr.ReadLine());
-        }
-        //TimeSpan ts = (DateTime.Now - dt);
-        //Console.Write(fi.FullName);
-        //Console.WriteLine(" [{0}]\t[{1}ms]", fi.Length, ts.TotalMilliseconds);
-      } finally {
-        if (sr != null) sr.Close();
-      }
-    }
+		public static void ReadFile(FileInfo fi)
+		{
+			//DateTime dt;
+			StreamReader sr = null;
+			try {
+				sr = new StreamReader(File.Open(fi.FullName, FileMode.Open));
+				currentLineNumber = 0;
+				//dt = DateTime.Now;
+				while (!sr.EndOfStream) {
+					currentLineNumber++;
+					ReadLine(sr.ReadLine());
+				}
+				//TimeSpan ts = (DateTime.Now - dt);
+				//Console.Write(fi.FullName);
+				//Console.WriteLine(" [{0}]\t[{1}ms]", fi.Length, ts.TotalMilliseconds);
+			} finally {
+				if (sr != null) {
+					sr.Close();
+				}
+			}
+		}
 
-    public static void ReadLine(string line)
-    {
-      try {
-        Psi.Parser.UnsafeParse(line);
-      } catch { }
-    }
+		public static void ReadLine(string line)
+		{
+			try {
+				Psi.Parser.UnsafeParse(line);
+			} catch {
+			}
+		}
 
-    public static void Run(string dir, int times)
-    {
-      for (int i = 0; i < times; i++) {
-        ReadDirectory(dir);
-      }
-    }
-  }
+		public static void Run(string dir, int times)
+		{
+			for (int i = 0; i < times; i++) {
+				ReadDirectory(dir);
+			}
+		}
+	}
 
-  public class Test
-  {
-    private static int currentLineNumber = -1;
-    private static FileInfo currentFile = null;
+	public class Test
+	{
+		private static int currentLineNumber = -1;
+		private static FileInfo currentFile = null;
 
-    public static void ReadDirectory(string directory)
-    {
-      DateTime dt = DateTime.Now;
-      DirectoryInfo di = new DirectoryInfo(directory);
+		public static void ReadDirectory(string directory)
+		{
+			DateTime dt = DateTime.Now;
+			DirectoryInfo di = new DirectoryInfo(directory);
+			
+			foreach (FileInfo fi in di.GetFiles()) {
+				ReadFile(fi);
+			}
+			
+			currentLineNumber = -1;
+			
+			Console.WriteLine("Total time: {0}s", (DateTime.Now - dt).TotalSeconds);
+		}
 
-      foreach (FileInfo fi in di.GetFiles()) {
-        ReadFile(fi);
-      }
+		public static void ReadFile(FileInfo fi)
+		{
+			currentFile = fi;
+			StreamReader sr = null;
+			try {
+				sr = new StreamReader(File.Open(fi.FullName, FileMode.Open));
+				currentLineNumber = 0;
+				while (!sr.EndOfStream) {
+					currentLineNumber++;
+					ReadLine(sr.ReadLine());
+				}
+			} finally {
+				if (sr != null) {
+					sr.Close();
+				}
+			}
+		}
 
-      currentLineNumber = -1;
+		public static void ReadLine(string line)
+		{
+			try {
+				Base log = Psi.Parser.UnsafeParse(line);
+				if (log != null) {
+					if (log.Log != line) {
+						Console.WriteLine("match failure!");
+						Console.WriteLine(log.Log);
+						Console.WriteLine(line);
+						Console.WriteLine();
+					}
+				}
+			} catch (Exception e) {
+				Console.WriteLine("{0}:{1} {2}", currentFile.Name, currentLineNumber, line);
+				Console.WriteLine(e.Message);
+				Console.WriteLine(e.StackTrace);
+				Console.WriteLine();
+			}
+		}
 
-      Console.WriteLine("Total time: {0}s", (DateTime.Now - dt).TotalSeconds);
-    }
+		public static void Run(string lines)
+		{
+			ReadDirectory(lines);
+		}
+	}
 
-    public static void ReadFile(FileInfo fi)
-    {
-      currentFile = fi;
-      StreamReader sr = null;
-      try {
-        sr = new StreamReader(File.Open(fi.FullName, FileMode.Open));
-        currentLineNumber = 0;
-        while (!sr.EndOfStream) {
-          currentLineNumber++;
-          ReadLine(sr.ReadLine());
-        }
-      } finally {
-        if (sr != null) sr.Close();
-      }
-    }
+	public class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length < 2) {
+				return;
+			}
 
-    public static void ReadLine(string line)
-    {
-      try {
-        Base log = Psi.Parser.UnsafeParse(line);
-        if (log != null) {
-          if (log.Log != line) {
-            Console.WriteLine("match failure!");
-            Console.WriteLine(log.Log);
-            Console.WriteLine(line);
-            Console.WriteLine();
-          }
-        }
-      } catch (Exception e) {
-        Console.WriteLine("{0}:{1} {2}", currentFile.Name, currentLineNumber, line);
-        Console.WriteLine(e.Message);
-        Console.WriteLine(e.StackTrace);
-        Console.WriteLine();
-      }
-    }
-
-    public static void Run(string lines)
-    {
-      ReadDirectory(lines);
-    }
-  }
-
-  public class MainClass
-  {
-    public static void Main(string[] args)
-    {
-      if (args.Length < 2)
-        return;
-
-      switch (args[0]) {
-      case "test":
-        Test.Run(args[1]);
-        break;
-      case "bench":
-        Benchmark.Run(args[1], 5);
-        break;
-      }
-
-    }
-  }
+			switch (args[0]) {
+			case "test":
+				Test.Run(args[1]);
+				break;
+			case "bench":
+				Benchmark.Run(args[1], 5);
+				break;
+			}
+		}
+	}
 }
--- README.md
+++ README.md
@@ -14,34 +14,7 @@ It is written in C# and completely by hand instead of using a parser generator f
 
 The code might seem little bit hacky, but it is efficient and well tested.
 
-Installation
-------------
-
-I'm using Mono 2.8 for development, but really every environment which supports .NET 2.0 and higher should be enough in order to build this small project. Just hit (x)build against psi.sln
-
-Help
-----
-
-If you need to talk to me, I'm available at andrius.bentkus@gmail.com or txdv @ irc.oftc.net, irc.freenode.org, irc.quakenet.org, irc.gimp.org.
-
-Coding guidelines
-----------------
-
-Use the [mono coding guidlines](http://www.mono-project.com/Coding_Guidelines), just use new lines for parenthesis after name spaces, and class definitions and skip the space between the method name and the opening parenthesis.
-
-Contributing
-------------
-
-If you need to use this library and you need to make some modifications feel free to contribute back.
-Just fork on github, make a merge request for the modifications.
-
 License
 -------
 
-This library is available under the [GPL3](http://www.gnu.org/licenses/gpl.html) license.
-
-
-Authors
--------
-
-* [Andrius Bentkus](mailto:andrius.bentkus@gmail.com)
+The library is available under the [MIT license](http://en.wikipedia.org/wiki/MIT_License).
