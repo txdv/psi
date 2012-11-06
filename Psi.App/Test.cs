3d39ce2d23a0f468c7b9e95e33d91d43eca9bec1
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-11-06 10:36:49 +0100

    Fix test.

--- Psi.App/Test.cs
+++ Psi.App/Test.cs
@@ -12,24 +12,13 @@ namespace Psi.App
 		{
 		}
 
+		RagelParser rp = new RagelParser();
+
 		public override void ReadLine(ArraySegment<byte> seg)
 		{
-			string line = Encoding.ASCII.GetString(seg.Array, seg.Offset, seg.Count);
-			try {
-				LogEvent log = MainClass.Parser.UnsafeParse(line);
-				if (log != null) {
-					if (log.Log != line) {
-						Console.WriteLine("match failure!");
-						Console.WriteLine(log.Log);
-						Console.WriteLine(line);
-						Console.WriteLine();
-					}
-				}
-			} catch (Exception e) {
-				Console.WriteLine("{0}:{1} {2}", CurrentFile.Name, CurrentLineNumber, line);
-				Console.WriteLine(e.Message);
-				Console.WriteLine(e.StackTrace);
-				Console.WriteLine();
+			if (!rp.Parse(seg)) {
+				string line = Encoding.ASCII.GetString(seg.Array, seg.Offset, seg.Count);
+				Console.WriteLine(line);
 			}
 		}
 	}
